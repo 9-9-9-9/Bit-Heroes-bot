3e99bb25fb1be088c387b077260010d5113c273e
9-9-9-9 <9-9-9-9>
2021-09-04 11:19:44 +0700

    fixed

--- src/main/java/bh/bot/app/dev/ImportTpImageApp.java
+++ src/main/java/bh/bot/app/dev/ImportTpImageApp.java
@@ -26,140 +26,136 @@ import static bh.bot.common.Log.warn;
 
 @AppCode(code = "tp")
 public class ImportTpImageApp extends AbstractApplication {
-    @Override
-    protected void internalRun(String[] args) {
-    	Configuration.Tolerant.colorBw = 60;
-    	warn("Forced value of Configuration.Tolerant.colorBw to %d", Configuration.Tolerant.colorBw);
-    	
-        int rgb = Integer.parseInt(args[0], 16) & 0xFFFFFF;
-        int tolerant = Configuration.Tolerant.colorBw;
+	@Override
+	protected void internalRun(String[] args) {
+		Configuration.Tolerant.colorBw = 60;
+		warn("Forced value of Configuration.Tolerant.colorBw to %d", Configuration.Tolerant.colorBw);
 
-        info("Keep RGB: %s", args[0]);
-        info("Tolerant: %d", tolerant);
-        info("Input file: %s", args[1]);
+		int rgb = Integer.parseInt(args[0], 16) & 0xFFFFFF;
+		int tolerant = Configuration.Tolerant.colorBw;
 
-        try {
-            String profileName;
-            String filePath;
-            final List<Opt> opts = Arrays.asList(
-                    new Opt("Button", "buttons"),
-                    new Opt("Dialog", "dialogs"),
-                    new Opt("Label", "labels")
-            );
-            try (
-                    InputStreamReader isr = new InputStreamReader(System.in);
-                    BufferedReader br = new BufferedReader(isr);
-            ) {
-                info("1. Web");
-                info("2. Steam");
-                profileName = readInput(br, "Which profile?", null, s -> {
-                    s = s.trim();
-                    if (s.equals("1"))
-                        return new Tuple3<>(true, null, "web");
-                    if (s.equals("2"))
-                        return new Tuple3<>(true, null, "steam");
-                    return new Tuple3<>(false, "Not supported", null);
-                });
+		info("Keep RGB: %s", args[0]);
+		info("Tolerant: %d", tolerant);
+		info("Input file: %s", args[1]);
 
-                String group = readInput(br, "Group?", "eg: globally", s -> {
-                    s = s.trim();
-                    if (StringUtil.isBlank(s))
-                        return new Tuple3<>(false, "Must not blank", null);
-                    return new Tuple3<>(true, null, s);
-                });
+		try {
+			String profileName;
+			String filePath;
+			final List<Opt> opts = Arrays.asList(new Opt("Button", "buttons"), new Opt("Dialog", "dialogs"),
+					new Opt("Label", "labels"));
+			try (InputStreamReader isr = new InputStreamReader(System.in);
+					BufferedReader br = new BufferedReader(isr);) {
+				profileName = readInput(br, "Which profile?\n  1. Web\n  2. Steam", null, s -> {
+					s = s.trim();
+					if (s.equals("1"))
+						return new Tuple3<>(true, null, "web");
+					if (s.equals("2"))
+						return new Tuple3<>(true, null, "steam");
+					return new Tuple3<>(false, "Not supported", null);
+				});
 
-                String name = readInput(br, "name?", "eg: town", s -> {
-                    s = s.trim();
-                    if (StringUtil.isBlank(s))
-                        return new Tuple3<>(false, "Must not blank", null);
-                    if (s.endsWith("bmp"))
-                        return new Tuple3<>(false, "Can not ends with 'bmp'", null);
-                    if (s.endsWith("-mx"))
-                        return new Tuple3<>(false, "Can not ends with '-mx'", null);
-                    if (s.endsWith("-tp"))
-                        return new Tuple3<>(false, "Can not ends with '-tp'", null);
-                    return new Tuple3<>(true, null, s);
-                });
+				String group = readInput(br, "Group?", "eg: globally", s -> {
+					s = s.trim();
+					if (StringUtil.isBlank(s))
+						return new Tuple3<>(false, "Must not blank", null);
+					return new Tuple3<>(true, null, s);
+				});
 
-                final String fileName = String.format("%s.%s-tp.bmp", group, name);
+				String name = readInput(br, "name?", "eg: town", s -> {
+					s = s.trim();
+					if (StringUtil.isBlank(s))
+						return new Tuple3<>(false, "Must not blank", null);
+					if (s.endsWith("bmp"))
+						return new Tuple3<>(false, "Can not ends with 'bmp'", null);
+					if (s.endsWith("-mx"))
+						return new Tuple3<>(false, "Can not ends with '-mx'", null);
+					if (s.endsWith("-tp"))
+						return new Tuple3<>(false, "Can not ends with '-tp'", null);
+					return new Tuple3<>(true, null, s);
+				});
 
-                for (int i = 0; i < opts.size(); i++)
-                    info("%d. %s", i + 1, opts.get(i).name);
+				final String fileName = String.format("%s.%s-tp.bmp", group, name);
 
-                filePath = readInput(br, "Which kind?", null, s -> {
-                    s = s.trim();
-                    int selected = Integer.parseInt(s);
-                    final String pn = profileName;
-                    Opt opt = opts.get(selected - 1);
-                    String[] mergedPath = Stream.concat(
-                            Arrays.asList("main", "resources", "game-images", pn).stream(),
-                            Arrays.asList(opt.path).stream()
-                    ).collect(Collectors.toList()).toArray(new String[0]);
-                    Path targetDir = Paths.get("src", mergedPath);
-                    File dir = targetDir.toFile();
-                    if (!dir.exists())
-                        return new Tuple3<>(false, String.format("Directory is not exists: %s", dir.getAbsolutePath()), null);
-                    File targetFile = Paths.get(dir.getAbsolutePath(), fileName).toFile();
-                    if (targetFile.exists())
-                        return new Tuple3<>(false, String.format("Target file is already exists: %s", targetFile.getAbsolutePath()), null);
-                    return new Tuple3<>(true, null, targetFile.getAbsolutePath());
-                });
-            } catch (IOException e) {
-                throw e;
-            }
+				StringBuilder sb = new StringBuilder("Which kind?\n");
+				for (int i = 0; i < opts.size(); i++)
+					sb.append(String.format("%d. %s\n", i + 1, opts.get(i).name));
 
-            BufferedImage bi = loadImageFromFile(args[1]);
+				filePath = readInput(br, sb.toString(), null, s -> {
+					s = s.trim();
+					int selected = Integer.parseInt(s);
+					final String pn = profileName;
+					Opt opt = opts.get(selected - 1);
+					String[] mergedPath = Stream
+							.concat(Arrays.asList("main", "resources", "game-images", pn).stream(),
+									Arrays.asList(opt.path).stream())
+							.collect(Collectors.toList()).toArray(new String[0]);
+					Path targetDir = Paths.get("src", mergedPath);
+					File dir = targetDir.toFile();
+					if (!dir.exists())
+						return new Tuple3<>(false, String.format("Directory is not exists: %s", dir.getAbsolutePath()),
+								null);
+					File targetFile = Paths.get(dir.getAbsolutePath(), fileName).toFile();
+					if (targetFile.exists())
+						return new Tuple3<>(false,
+								String.format("Target file is already exists: %s", targetFile.getAbsolutePath()), null);
+					return new Tuple3<>(true, null, targetFile.getAbsolutePath());
+				});
+			} catch (IOException e) {
+				throw e;
+			}
 
-            ImageUtil.TestTransformMxResult testTransformMxResult = ImageUtil.testTransformMx(bi, rgb, tolerant);
+			BufferedImage bi = loadImageFromFile(args[1]);
 
-            File file = new File(filePath);
-            if (file.exists())
-                throw new InvalidDataException("File exists!!!");
-            ImageIO.write(testTransformMxResult.tp, "bmp", file);
+			ImageUtil.TestTransformMxResult testTransformMxResult = ImageUtil.testTransformMx(bi, rgb, tolerant);
 
-            info("Saved TP image with name '%s' to %s", file.getName(), file.getAbsolutePath());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			File file = new File(filePath);
+			if (file.exists())
+				throw new InvalidDataException("File exists!!!");
+			ImageIO.write(testTransformMxResult.tp, "bmp", file);
 
-    private static class Opt {
-        public final String name;
-        public final String[] path;
+			info("Saved TP image with name '%s' to %s", file.getName(), file.getAbsolutePath());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-        private Opt(String name, String... path) {
-            this.name = name;
-            this.path = path;
-        }
-    }
+	private static class Opt {
+		public final String name;
+		public final String[] path;
 
-    @Override
-    protected String getAppName() {
-        return "BH-Import TP Image";
-    }
+		private Opt(String name, String... path) {
+			this.name = name;
+			this.path = path;
+		}
+	}
 
-    @Override
-    protected String getScriptFileName() {
-        return "tp";
-    }
+	@Override
+	protected String getAppName() {
+		return "BH-Import TP Image";
+	}
 
-    @Override
-    protected String getUsage() {
-        return "<KeepHexColor> <image>";
-    }
+	@Override
+	protected String getScriptFileName() {
+		return "tp";
+	}
 
-    @Override
-    protected String getDescription() {
-        return "(developers only) Import tp image";
-    }
+	@Override
+	protected String getUsage() {
+		return "<KeepHexColor> <image>";
+	}
 
-    @Override
-    protected String getLimitationExplain() {
-        return null;
-    }
+	@Override
+	protected String getDescription() {
+		return "(developers only) Import tp image";
+	}
 
-    @Override
-    protected boolean isRequiredToLoadImages() {
-        return false;
-    }
+	@Override
+	protected String getLimitationExplain() {
+		return null;
+	}
+
+	@Override
+	protected boolean isRequiredToLoadImages() {
+		return false;
+	}
 }
