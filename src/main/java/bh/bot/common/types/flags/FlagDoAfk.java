7db1e4b4175800654bb0e301084f8dd467a5e38c
9-9-9-9 <9-9-9-9>
2021-09-10 21:21:05 +0700

    'update'

--- src/main/java/bh/bot/common/types/flags/FlagDoAfk.java
+++ src/main/java/bh/bot/common/types/flags/FlagDoAfk.java
@@ -4,21 +4,22 @@ import bh.bot.app.AbstractApplication;
 import bh.bot.app.AfkApp;
 import bh.bot.common.exceptions.InvalidFlagException;
 import bh.bot.common.exceptions.NotSupportedException;
+import bh.bot.common.utils.StringUtil;
 
 public class FlagDoAfk extends FlagPattern<FlagDoAfk.AfkBatch> {
-    public static final char codePvp = 'P';
-    public static final char codeWorldBoss1 = 'B';
-    public static final char codeWorldBoss2 = 'W';
-    public static final char codeRaid = 'R';
-    public static final char codeInvasion = 'I';
-    public static final char codeExpedition = 'E';
-    public static final char codeGVG = 'V';
-    public static final char codeTrials = 'T';
-    public static final char codeGauntlet = 'G';
-    public static final char codeComboInvasionGvgExpedition = '1';
-    public static final char codeComboTrialsGauntlet = '2';
-    public static final char codeComboPvpWorldBossRaid = '3';
-    public static final char codeComboAll = 'A';
+    private static final char codePvp = 'P';
+    private static final char codeWorldBoss1 = 'B';
+    private static final char codeWorldBoss2 = 'W';
+    private static final char codeRaid = 'R';
+    private static final char codeInvasion = 'I';
+    private static final char codeExpedition = 'E';
+    private static final char codeGVG = 'V';
+    private static final char codeTrials = 'T';
+    private static final char codeGauntlet = 'G';
+    private static final char codeComboPvpWorldBossRaid = '1';
+    private static final char codeComboInvasionGvgExpedition = '2';
+    private static final char codeComboTrialsGauntlet = '3';
+    private static final char codeComboAll = 'A';
 
     @Override
     public boolean isGlobalFlag() {
@@ -32,10 +33,10 @@ public class FlagDoAfk extends FlagPattern<FlagDoAfk.AfkBatch> {
 
     @Override
     public String getDescription() {
-        return String.format("Short way to include tasks to do by AFK. Accepted values are combination of: %s", shortDesc);
+        return String.format("Short way to include tasks to do by AFK. Accepted values are combination of: %s. For example: `%s=%s%s` means you want to do PVP and GVG or `%s=%s` means you want to do everything", shortDesc, getCode(), codePvp, codeGVG, getCode(), codeComboAll);
     }
 
-    private final String shortDesc = String.format("%s (PVP), %s (World Boss), %s (Raid), %s (Invasion), %s (Expedition), %s (GVG), %s (Gauntlet), %s (Trials), %s (Invasion/GVG/Expedition), %s (Trials/Gauntlet), %s (PVP/World Boss/Raid), %s (All)", codePvp, codeWorldBoss1, codeRaid, codeInvasion, codeExpedition, codeGVG, codeGauntlet, codeTrials, codeComboInvasionGvgExpedition, codeComboTrialsGauntlet, codeComboPvpWorldBossRaid, codeComboAll);
+    private final String shortDesc = String.format("%s (PVP), %s (World Boss), %s (Raid), %s (Invasion), %s (Expedition), %s (GVG), %s (Gauntlet), %s (Trials), %s (PVP/World Boss/Raid), %s (Invasion/GVG/Expedition), %s (Trials/Gauntlet), %s (All)", codePvp, codeWorldBoss1, codeRaid, codeInvasion, codeExpedition, codeGVG, codeGauntlet, codeTrials, codeComboPvpWorldBossRaid, codeComboInvasionGvgExpedition, codeComboTrialsGauntlet, codeComboAll);
 
     @Override
     public boolean isAllowParam() {
@@ -49,8 +50,13 @@ public class FlagDoAfk extends FlagPattern<FlagDoAfk.AfkBatch> {
 
     @Override
     protected AfkBatch internalParseParam(String paramPart) throws InvalidFlagException {
-        AfkBatch result = new AfkBatch();
-        for (char c : paramPart.toUpperCase().trim().toCharArray()) {
+        final String normalized = paramPart.toUpperCase().trim();
+        final AfkBatch result = new AfkBatch();
+
+        if (StringUtil.isBlank(normalized))
+            throw new InvalidFlagException(String.format("Flag `%s` requires parameter. Valid values are combination of: %s", getCode(), shortDesc));
+
+        for (char c : normalized.toCharArray()) {
             if (c == codeComboAll) {
                 result.doPvp = true;
                 result.doWorldBoss = true;
