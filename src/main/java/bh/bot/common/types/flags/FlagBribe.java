9eda0646134a0d1eb12ce8d5e9a2f46892aebc32
9-9-9-9 <9-9-9-9>
2021-09-19 16:13:12 +0700

    add textures

---
+++ src/main/java/bh/bot/common/types/flags/FlagBribe.java
@@ -0,0 +1,59 @@
+package bh.bot.common.types.flags;
+
+import bh.bot.app.AbstractApplication;
+import bh.bot.app.AfkApp;
+import bh.bot.app.ReRunApp;
+import bh.bot.app.farming.AbstractDoFarmingApp;
+import bh.bot.common.exceptions.InvalidFlagException;
+import bh.bot.common.exceptions.NotSupportedException;
+import bh.bot.common.types.Familiar;
+
+public class FlagBribe extends FlagPattern<Familiar> {
+
+	@Override
+	protected Familiar internalParseParam(String paramPart) throws InvalidFlagException {
+		String[] spl = paramPart.toLowerCase().split("[\\,\\;]");
+		for (String s : spl) {
+			switch (s) {
+			case "kaleido":
+				return Familiar.Kaleido;
+			default:
+				throw new NotSupportedException(String.format("Unknown value '%s' of flag %s", s, getCode()));
+			}
+		}
+		throw new InvalidFlagException(String.format("Passing invalid value for flag %s", getCode()));
+	}
+
+	@Override
+	public boolean isAllowParam() {
+		return true;
+	}
+
+	@Override
+	protected boolean isAllowMultiple() {
+		return true;
+	}
+
+	@Override
+	public boolean isGlobalFlag() {
+		return false;
+	}
+
+	@Override
+	public String getName() {
+		return "bribe";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Auto bribe with gems";
+	}
+	
+	@Override
+	protected boolean internalCheckIsSupportedByApp(AbstractApplication instance) {
+		return instance instanceof AfkApp
+				|| instance instanceof ReRunApp
+				|| instance instanceof AbstractDoFarmingApp;
+	}
+
+}
