007f8b334fa1ae11a9c4d3e84ccebede08126566
9-9-9-9 <49741222+9-9-9-9@users.noreply.github.com>
2021-09-21 14:03:17 +0700

    update

--- _.bat
+++ _.bat
@@ -4,6 +4,7 @@ where java >nul 2>nul
 if %errorlevel%==1 (
     @echo ** ERROR ** You have to install Java 8 in order to use this bot
     @echo ** LOI ** Ban can cai dat Java 8 truoc khi su dung
+	@echo You can download Java 8 from the following website: https://adoptium.net/?variant=openjdk8
 	start "" https://adoptium.net/?variant=openjdk8
     goto L_EXIT
 )
