2a232d00d4800f284c1e39bdc9a3f4b972366035
9-9-9-9 <9-9-9-9>
2021-10-04 01:38:27 +0700

    change ver 2.0.0 & add @Deprecated to --web and --steam

--- src/main/java/bh/bot/common/types/flags/FlagSteamResolution800x480.java
+++ src/main/java/bh/bot/common/types/flags/FlagSteamResolution800x480.java
@@ -4,6 +4,7 @@ import bh.bot.common.types.Platform;
 import bh.bot.common.types.annotations.FlagMeta;
 
 @FlagMeta(cbDesc = "Using Steam with game resolution 800x480", checked = true, displayOrder = 1)
+@Deprecated
 public class FlagSteamResolution800x480 extends FlagResolution {
     @Override
     public String getName() {
@@ -12,7 +13,7 @@ public class FlagSteamResolution800x480 extends FlagResolution {
 
     @Override
     public String getDescription() {
-        return "When game resolution 800x480 while playing on Steam client";
+        return "(Deprecated from version 2.0.0 due to game window will be resized to 800x520 resolution) When game resolution 800x480 while playing on Steam client";
     }
 
     public Platform[] getSupportedOsPlatforms() {
--- src/main/java/bh/bot/common/types/flags/FlagWebResolution800x520.java
+++ src/main/java/bh/bot/common/types/flags/FlagWebResolution800x520.java
@@ -1,5 +1,6 @@
 package bh.bot.common.types.flags;
 
+@Deprecated
 public class FlagWebResolution800x520 extends FlagResolution {
     @Override
     public String getName() {
@@ -8,6 +9,6 @@ public class FlagWebResolution800x520 extends FlagResolution {
 
     @Override
     public String getDescription() {
-        return "(default) When game resolution 800x520 while playing on website";
+        return "(Deprecated from 2.0.0 due to unite resolution to 800x520 thus no need to provide this flag) (default) When game resolution 800x520 while playing on website";
     }
 }
