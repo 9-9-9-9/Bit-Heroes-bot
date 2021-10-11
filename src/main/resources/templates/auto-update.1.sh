038a5bc71c8ac88b7f6378cd8a912b591e1f6007
9-9-9-9 <9-9-9-9>
2021-10-11 18:05:21 +0700

    'update'

--- src/main/resources/templates/auto-update.1.sh
+++ src/main/resources/templates/auto-update.1.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+print_warning_and_exit() {
+  echo '** ERROR ** Failed while attempting to patch the file '$2' from downloaded file '$1
+  echo '** ERROR ** Patch new update %VERSION% has failure, please update manually yourself by going to https://download.bh99bot.com'
+  echo 'Sorry for this inconvenient'
+  exit 1
+}
+
 patch_file() {
   SRC=$1
   DST=$2
@@ -8,13 +17,11 @@ patch_file() {
   fi
 
   cp $SRC $DST
-}
 
-print_warning_and_exit() {
-  echo '** ERROR ** Failed while attempting to patch the file '$2' from downloaded file '$1
-  echo '** ERROR ** Patch new update %VERSION% has failure, please update manually yourself by going to https://download.bh99bot.com'
-  echo 'Sorry for this inconvenient'
-  exit 1
+  err=$?
+  if [ $err -ne 0 ]; then
+    print_warning_and_exit %SRC% %DST%
+  fi
 }
 
 %COPY_SCRIPT%
