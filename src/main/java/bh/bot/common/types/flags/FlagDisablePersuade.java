b8486e1fac2372975b47388f6d74a560129592a3
9-9-9-9 <9-9-9-9>
2021-10-13 02:40:57 +0700

    new flag `--disable-persuade`

--- src/main/java/bh/bot/common/types/flags/FlagDisablePersuade.java
+++ src/main/java/bh/bot/common/types/flags/FlagDisablePersuade.java
@@ -4,7 +4,9 @@ import bh.bot.app.AbstractApplication;
 import bh.bot.app.AfkApp;
 import bh.bot.app.ReRunApp;
 import bh.bot.app.farming.AbstractDoFarmingApp;
+import bh.bot.common.types.annotations.FlagMeta;
 
+@FlagMeta(displayOrder = 9)
 public class FlagDisablePersuade extends FlagPattern.NonParamFlag {
     @Override
     public boolean isGlobalFlag() {
@@ -18,7 +20,7 @@ public class FlagDisablePersuade extends FlagPattern.NonParamFlag {
 
     @Override
     public String getDescription() {
-        return "Disable auto persuade & bribe";
+        return "Disable auto persuade & bribe (by default, bot will auto persuade with gold all familiars if your did not setting auto persuade with gold)";
     }
 
     @Override
