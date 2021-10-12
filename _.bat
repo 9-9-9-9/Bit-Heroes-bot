ed9bb84cb2e028f2e6d8e22c54c172c6836e9efe
9-9-9-9 <9-9-9-9>
2021-10-12 18:19:16 +0700

    'update'

--- _.bat
+++ _.bat
@@ -6,15 +6,19 @@
 @where java >nul 2>nul
 @if %errorlevel%==1 (
     @echo ** ERROR ** You have to install Java 8 in order to use this bot
-    @echo ** LOI ** Ban can cai dat Java 8 truoc khi su dung
 	@echo You can download Java 8 from the following website: https://adoptium.net/?variant=openjdk8
 	start "" https://bh99bot.com/require-java.html
-    goto L_EXIT
+    goto L_EXIT1
 )
 
 java -jar BitHeroes.jar %*
 
-@goto L_EXIT
+@goto L_EXIT0
 
-:L_EXIT
+:L_EXIT0
 	@pause
+	exit /b 0
+
+:L_EXIT1
+	@pause
+	exit /b 1
