ea5463125188c0b1728fe029131414e20b1a121c
9-9-9-9 <9-9-9-9>
2022-10-30 17:45:09 +0700

    adjust World Boss description

--- src/main/java/bh/bot/common/types/UserConfig.java
+++ src/main/java/bh/bot/common/types/UserConfig.java
@@ -167,9 +167,9 @@ public class UserConfig {
             case 6:
                 return "Titans Attack! (T11-T16)";
             case 7:
-                return "The Ignited Abyss (T13-T16)";
+                return "The Ignited Abyss (T13-T18)";
             case 8:
-                return "Nordic Dream (T16-T18)";
+                return "Nordic Dream (T16-T19)";
             case 9:
                 return "Notorious Beef (T18-T19)";
             default:
