fa8f1d85f8c054d3a223e358ed4e5b13b1e5a603
DARE <me@daredoes.work>
2023-01-19 17:38:35 -0800

    Add bmp

---
+++ ReRun.web.bat
@@ -0,0 +1,7 @@
+:: Do chaining tasks PVP, WB, GVG/Invasion/Expedition, TG, Raid and then exit after completed them all
+:: https://github.com/9-9-9-9/Bit-Heroes-bot/wiki/Function-%22afk%22
+:: Consider adding `--profile=YourProfileName` so you no longer needed to select profile manually
+:: You can remove flag `--ear` to keep game online all day (but still gone if you got Disconnected)
+:: With steam version of BH, refer to AFK.steam.bat file
+call web.bot.bat quest 3 --debug --img --profile=dare
+:: Move this file to bot's folder in order to use (this file was distributed within `sample-script` folder so it unable to run)
