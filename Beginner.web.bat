ed1184190d02e65606b47d497edc002fa96ec08d
Daniel Evans <me@daredoes.work>
2023-01-26 20:51:31 -0800

    remove unused files

--- Beginner.web.bat
+++
@@ -1,6 +0,0 @@
-:: Do chaining tasks PVP, WB, GVG/Invasion/Expedition, TG, Raid and then exit after completed them all
-:: https://github.com/9-9-9-9/Bit-Heroes-bot/wiki/Function-%22afk%22
-:: Consider adding `--profile=YourProfileName` so you no longer needed to select profile manually
-:: You can remove flag `--ear` to keep game online all day (but still gone if you got Disconnected)
-:: With steam version of BH, refer to AFK.steam.bat file
-call web.bot.bat afk a --ear --profile=newb
--- Fishing.web.bat
+++
@@ -1,7 +0,0 @@
-:: Do fishing
-:: https://github.com/9-9-9-9/Bit-Heroes-bot/wiki/Function-%22fishing%22
-:: With steam version of BH, refer to Fishing.steam.bat file
-call web.bot.bat fishing 500
-:: ARGUMENTS: (you can adjust to fit your need)
-:: First argument: 500: hook 500 times
-:: Move this file to bot's folder in order to use (this file was distributed within `sample-script` folder so it unable to run)
